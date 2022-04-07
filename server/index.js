3d9e9fb7047717c00ae82631ad3e5edbcf2f12cd
Akhil <akhillav@gmail.com>
2022-04-07 17:36:57 +0530

    env, google auth

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("./passportConfig");
 
-const AuthRoute = require("./routes/Auth");
+require("dotenv").config();
+
 const SearchRoute = require("./routes/Search");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.status(401).send({ message: "not_logged_in" });
+}
+
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/auth", AuthRoute);
-app.use("/searchs", SearchRoute);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] }),
+);
+
+app.use("/searches", SearchRoute);
 
 app.listen(PORT, () => console.log(`Server is running at PORT: ${PORT}`));
---
+++ server/passportConfig.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://yourdomain:3000/auth/google/callback",
+    passReqToCallback: true,
+  },
+  ((request, accessToken, refreshToken, profile, done) => {
+    // User.findOrCreate({ googleId: profile.id }, (err, user) => done(err, user));
+    return done(null, profile);
+  }),
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
